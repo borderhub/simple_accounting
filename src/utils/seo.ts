3b596c6d8e209de161a89d115161ffc27fdb39ae
“borderhub” <shirabegroup@gmail.com>
2025-02-28 23:28:44 +0900

    内容修正

--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -1,7 +1,7 @@
 export const defaultMeta = {
-    title: '福岡市南区大橋の美容室 Hair Resort un DEUX',
-    description: 'un DEUX（アンドゥ）は福岡市南区大橋を拠点にしている美容室。',
-    url: 'https://undeux-fukuoka.com/',
+    title: 'アートスペース・テトラ | art space tetra',
+    description: 'アートスペース・テトラは、福岡市の中心部でありながら枯れた情緒を残す博多区須崎町に2004年4月、オープンいたしました。 企画者や美術家、音楽家、デザイナー等による共同経営',
+    url: 'http://www.as-tetra.info/',
     image: 'img/bg.jpeg',
   };
   
